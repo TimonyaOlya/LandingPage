2bf66cace60213d4773308935b9069cb9d24b8a0
Ольга <80743132+TimonyaOlya@users.noreply.github.com>
2021-10-23 20:13:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,122 @@
+const value = {
+    'Landing Page': {
+        pm: 700,
+        design: 600,
+        development: 1200,
+        qa: 500,
+    },
+    'Online Store': {
+        pm: 1200,
+        design: 900,
+        development: 2500,
+        qa: 800,
+    },
+    'Web Application': {
+        pm: 2000,
+        design: 1100,
+        development: 3000,
+        qa: 1000,
+    },
+    'Mobile Application': {
+        pm: 3000,
+        design: 1500,
+        development: 4000,
+        qa: 1300,
+    }
+};
+
+function getFormValues() {
+
+    const websiteTypeElement = document.querySelector('#project_type_id');
+    const pmEl = document.querySelector('#pm');
+    const designEl = document.querySelector('#design');
+    const developmentEl = document.querySelector('#development');
+    const qaEl = document.querySelector('#qa');
+
+    return {
+        websiteType: websiteTypeElement.value,
+        pm: pmEl.checked,
+        design: designEl.checked,
+        development: developmentEl.checked,
+        qa: qaEl.checked,
+    }
+
+}
+
+function calculate() {
+    const values = getFormValues();
+    let totalPrice = 0;
+
+    const workTypes = value[values.websiteType];
+    if (values.pm) {
+        totalPrice = workTypes.pm;
+    }
+    if (values.design) {
+        totalPrice = totalPrice + workTypes.design;
+    }
+
+    if (values.development) {
+        totalPrice = totalPrice + workTypes.development;
+    }
+
+    if (values.qa) {
+        totalPrice = totalPrice + workTypes.qa;
+    }
+
+    const totalPriceEl = document.querySelector('#totalPrice');
+
+    totalPriceEl.textContent = totalPrice;
+
+    console.log(totalPrice);
+
+}
+const formEl = document.querySelector('#calculationForm');
+const emailModal = document.querySelector('#modal-email');
+const successModal = document.querySelector('#modal-success');
+const inputContainer = document.querySelector('#user-email');
+const textError = document.querySelector('#text-email-error');
+const userEmailInput = document.querySelector('#user-email');
+
+calculate();
+
+formEl.addEventListener('change', calculate);
+
+formEl.addEventListener('submit', function(event) {
+    event.preventDefault()
+    emailModal.classList.add('modal-active');
+});
+
+const closeButtons = document.querySelectorAll('.modal-close-icon');
+
+closeButtons.forEach(function(closeButton) {
+    closeButton.addEventListener('click', function() {
+        emailModal.classList.remove('modal-active');
+        successModal.classList.remove('modal-active');
+        inputContainer.classList.remove('user-email-error');
+        textError.classList.remove('text-email-error');
+
+    });
+});
+const modalContainerEmail = document.querySelector('#modal-container-email');
+
+modalContainerEmail.addEventListener('submit', function(event) {
+    event.preventDefault();
+    if (userEmailInput.value) {
+        emailModal.classList.remove('modal-active');
+
+        const formData = new FormData(formEl);
+        formData.append('Email', userEmailInput.value);
+        fetch('/', {
+                method: 'POST',
+                headers: { "Content-Type": "application/x-www-form-urlencoded" },
+                body: new URLSearchParams(formData).toString(),
+            })
+            .then(function() {
+
+                emailModal.classList.remove('modal-active');
+                successModal.classList.add('modal-active');
+
+            })
+            .catch((error) => alert('не удалось отправить форму'));
+    }
+});
